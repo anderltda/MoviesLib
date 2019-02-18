eedca23a9a7959a4d4321bbc1176ae961b1a723c
Usuário Convidado <Guest@pa06micro03.local>
2019-02-18 20:42:04 -0300

    ok

---
+++ MoviesLib/MovieViewController.swift
@@ -0,0 +1,38 @@
+//
+//  ViewController.swift
+//  MoviesLib
+//
+//  Created by Usuário Convidado on 11/02/19.
+//  Copyright © 2019 FIAP. All rights reserved.
+
+// app icon generator ios
+
+import UIKit
+
+class MovieViewController: UIViewController {
+    
+    var movie: Movie?
+    
+    @IBOutlet weak var ivPoster: UIImageView!
+    @IBOutlet weak var lbTitle: UILabel!
+    @IBOutlet weak var lbDuration: UILabel!
+    @IBOutlet weak var lbRating: UILabel!
+    @IBOutlet weak var lbCategories: UILabel!
+    @IBOutlet weak var tvSummary: UITextView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()        
+        if let movie = movie {
+            ivPoster.image = UIImage(named: movie.image)
+            lbTitle.text = movie.title
+            lbDuration.text = movie.duration
+            lbRating.text = "⭐️\(movie.rating)/10"
+            lbCategories.text = movie.categories
+            tvSummary.text = movie.summary
+        }
+    }
+    
+}
+
+
+
